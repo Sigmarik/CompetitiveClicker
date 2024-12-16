7a2b7445b2175927b8a801321d799ee31d8ae43b
foxidokun <foxido@foxido.dev>
2024-12-16 15:47:16 +0300

    Custom player spawning

---
+++ Assets/Network/ClickerNetworkManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public struct CreateCharacterMessage : NetworkMessage
+{
+    public Team team;
+}
+
+public class ClickerNetworkManager : NetworkManager
+{
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+
+        NetworkServer.RegisterHandler<CreateCharacterMessage>(OnCreateCharacter);
+    }
+
+    public override void OnClientConnect()
+    {
+        base.OnClientConnect();
+
+        CreateCharacterMessage characterMessage = new CreateCharacterMessage
+        {
+            team = Team.Goblins
+        };
+
+        NetworkClient.Send(characterMessage);
+    }
+
+    void OnCreateCharacter(NetworkConnectionToClient conn, CreateCharacterMessage message)
+    {
+        // playerPrefab is the one assigned in the inspector in Network
+        // Manager but you can use different prefabs per race for example
+        GameObject gameobject = Instantiate(playerPrefab);
+
+        // Apply data from the message however appropriate for your game
+        // Typically Player would be a component you write with syncvars or properties
+        PlayerControl player = gameobject.GetComponent<PlayerControl>();
+
+        // TODO
+
+        gameobject.transform.position = new Vector3(0.0f, 4.0f, 2.0f);
+
+        // call this to use this gameobject as the primary controller
+        NetworkServer.AddPlayerForConnection(conn, gameobject);
+    }
+}
