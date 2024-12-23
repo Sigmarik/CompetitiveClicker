3842e3aef20a3e1a9f99ce4b21c3908adb3075f2
Daniil <penkovdaniil1902@yandex.ru>
2024-12-24 00:04:21 +0300

    Syncronization fix

--- Assets/Prefabs/Player/Perks/Perks.cs
+++ Assets/Prefabs/Player/Perks/Perks.cs
@@ -1,5 +1,5 @@
 public static class Perks {
     
     public static Perk Debug = new Perk("Debug", 1);
-    public static Perk Speed = new Perk("Speed", 1000);
+    public static Perk Speed = new Perk("Speed", 10);
 }
